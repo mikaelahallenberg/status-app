87337bc8a3cdfb786a66f2bf1c5c6a4a5381e60b
mikaelahallenberg <mikaelahallenberg@gmail.com>
2019-10-09 12:27:17 +0300

    Updates

---
+++ precache-manifest.86987f95ca039f2c6e3547496f6d1123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f168a82f17dea8e2b7e8e406ef5b5548",
+    "url": "/status-app/index.html"
+  },
+  {
+    "revision": "a5465e6b418a75d120dc",
+    "url": "/status-app/static/css/main.a8b8a9e4.chunk.css"
+  },
+  {
+    "revision": "a21b8a8ebda424aae2e7",
+    "url": "/status-app/static/js/2.05ae4e6d.chunk.js"
+  },
+  {
+    "revision": "a5465e6b418a75d120dc",
+    "url": "/status-app/static/js/main.13a4943c.chunk.js"
+  },
+  {
+    "revision": "c8d2b2c76f6497f636e6",
+    "url": "/status-app/static/js/runtime-main.f04bad71.js"
+  }
+]);
